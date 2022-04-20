f635e2ea51a87d579defb9d6faaec20ee22ffff6
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2022-04-20 23:56:07 +1000

    sigh

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -21,6 +21,7 @@ import VueRenderPlugin from "rete-vue-render-plugin";
 import ContextMenuPlugin from "rete-context-menu-plugin";
 import AreaPlugin from "rete-area-plugin";
 import VueNumControl from "./NumControl.vue";
+import VueTextControl from "./TextControl.vue";
 import ModulePlugin from "rete-module-plugin";
 import DockPlugin from "rete-dock-plugin";
 
@@ -34,11 +35,24 @@ export default {
     var numSocket = new Socket("Number value");
     var stringSocket = new Socket("String value");
     var dateSocket = new Socket("Date value");
+    var boolSocket = new Socket("Bool value");
 
     class NumControl extends Control {
       constructor(emitter, key, readonly) {
         super(key);
         this.component = VueNumControl;
+        this.props = { emitter, ikey: key, readonly };
+      }
+
+      setValue(val) {
+        this.vueContext.value = val;
+      }
+    }
+
+    class TextControl extends Control {
+      constructor(emitter, key, readonly) {
+        super(key);
+        this.component = VueTextControl;
         this.props = { emitter, ikey: key, readonly };
       }
 
@@ -101,6 +115,7 @@ export default {
     class InvoiceComponent extends Component {
       constructor() {
         super("Invoice");
+
       }
 
       builder(node) {
@@ -130,7 +145,8 @@ export default {
         var invoiceId = new Output("invoiceId", "InvoiceId", stringSocket);
         var accountId = new Output("accountId", "AccountId", stringSocket);
         var amountDue = new Output("amountDue", "AmountDue", numSocket);
-        var dueDate = new Output("dueDate", "DueDate", dateSocket);
+        var dueDate = new Output("dueDate", "DueDate", stringSocket);
+        var otherDate = new Output("otherDate", "OtherDate", stringSocket);
         var accountName = new Output(
           "accountName",
           "AccountName",
@@ -141,8 +157,53 @@ export default {
           .addOutput(invoiceId)
           .addOutput(accountId)
           .addOutput(amountDue)
+          .addOutput(accountName)
           .addOutput(dueDate)
-          .addOutput(accountName);
+          .addOutput(otherDate);
+      }
+
+      worker() {}
+    }
+
+    class DateParseComponent extends Component {
+      constructor() {
+        super("DateParse");
+      }
+
+      builder(node) {
+        var input = new Input("input", "Input", stringSocket);
+        var dateFormat = new Input("dateFormat", "DateFormat", stringSocket);
+        var dateOutput = new Output("dateOutput", "dateOutput", dateSocket);
+        var isValid = new Output("isValid", "isValid", boolSocket);
+
+        dateFormat.addControl(new TextControl(this.editor, "dateFormat"));
+
+        return node
+          .addInput(input)
+          .addInput(dateFormat)
+          .addOutput(dateOutput)
+          .addOutput(isValid);
+      }
+
+      worker() {}
+    }
+
+    class ConditionalDateComponent extends Component {
+      constructor() {
+        super("ConditionalDate");
+      }
+
+      builder(node) {
+        var input = new Input("input", "Input", boolSocket);
+        var trueValue = new Input("trueValue", "TrueValue", dateSocket);
+        var falseValue = new Input("falseValue", "FalseValue", dateSocket);
+        var outValue = new Output("outValue", "OutValue", dateSocket);
+
+        return node
+          .addInput(input)
+          .addInput(trueValue)
+          .addInput(falseValue)
+          .addOutput(outValue);
       }
 
       worker() {}
@@ -154,6 +215,8 @@ export default {
       new AddComponent(),
       new InvoiceComponent(),
       new FileInputComponent(),
+      new DateParseComponent(),
+      new ConditionalDateComponent()
     ];
 
     var editor = new NodeEditor("demo@0.1.0", container);
@@ -178,8 +241,16 @@ export default {
     var dest = await components[2].createNode();
     var srcFile = await components[3].createNode();
 
+    dest.position = [400, 0];
+    srcFile.position = [-400, 0];
+
     editor.addNode(dest);
     editor.addNode(srcFile);
+
+    editor.connect(srcFile.outputs.get('invoiceId'), dest.inputs.get('invoiceId'));
+    editor.connect(srcFile.outputs.get('accountId'), dest.inputs.get('accountId'));
+    editor.connect(srcFile.outputs.get('amountDue'), dest.inputs.get('amountDue'));
+    editor.connect(srcFile.outputs.get('accountName'), dest.inputs.get('accountName'));
 
     editor.on(
       "process nodecreated noderemoved connectioncreated connectionremoved",
